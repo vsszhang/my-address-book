09a534dab8aa048940f912f8e6888171d4bb0b5b
vsszhang <vsszhang@gmail.com>
2022-09-13 14:19:47 +0800

    re-destruct and add orm config

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,15 +1,9 @@
-import { App, Configuration } from '@midwayjs/decorator';
-import { ILifeCycle } from '@midwayjs/core';
-import { Application } from 'egg';
+import { Configuration } from '@midwayjs/decorator';
+import * as orm from '@midwayjs/orm';
 import { join } from 'path';
 
 @Configuration({
+  imports: [orm],
   importConfigs: [join(__dirname, './config')],
-  conflictCheck: true,
 })
-export class ContainerLifeCycle implements ILifeCycle {
-  @App()
-  app: Application;
-
-  async onReady() {}
-}
+export class ContainerConfiguratin {}
