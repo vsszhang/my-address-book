a3aa1ecae1458105bfc28964370815eebc3610ae
vsszhang <vsszhang@gmail.com>
2022-09-14 10:09:55 +0800

    add find and remove controller

--- src/controller/AddressBookController.ts
+++ src/controller/AddressBookController.ts
@@ -2,8 +2,10 @@ import {
   ALL,
   Body,
   Controller,
+  Del,
   Get,
   Inject,
+  Param,
   Post,
   Provide,
 } from '@midwayjs/decorator';
@@ -16,12 +18,21 @@ export class AddressBookController {
   @Inject()
   addrssBookService: AddressBookService;
 
-  @Get('/get')
-  async getInfo() {}
+  @Get('/find-by-address/:address')
+  async findByAddress(@Param() address: string) {
+    const findRequest = this.addrssBookService.findByAddress(address);
+    return findRequest;
+  }
 
   @Post('/add')
   async addItem(@Body(ALL) addressBookRequest: AddressBookRequest) {
     const saveRequest = this.addrssBookService.saveAddress(addressBookRequest);
     return saveRequest;
+  }
+
+  @Del('/remove-by-address/:address')
+  async removeByAddress(@Param() address: string) {
+    const removeRequest = this.addrssBookService.deleteByAddress(address);
+    return removeRequest;
   }
 }
